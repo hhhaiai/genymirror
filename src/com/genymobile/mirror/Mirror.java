b6dab8b64c36cd00dee509f4d622b03ca2e25124
sanbo <sanbo.xyz@gmail.com>
2021-02-02 18:23:32 +0800

    genymobile 反射库

---
+++ src/com/genymobile/mirror/Mirror.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Genymobile
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.genymobile.mirror;
+
+import java.lang.reflect.Proxy;
+
+public final class Mirror {
+
+    public static <T> T create(Class<T> clazz) {
+        return create(clazz, clazz.getClassLoader());
+    }
+
+    public static <T> T create(Class<T> clazz, ClassLoader targetClassLoader) {
+        Unwrapper unwrapper = new Unwrapper(targetClassLoader);
+        ReflectionFinder finder = new ReflectionFinder(targetClassLoader, unwrapper);
+        Wrapper wrapper = new Wrapper(targetClassLoader, finder);
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(),
+                new Class<?>[]{clazz},
+                new MirrorHandler(clazz, targetClassLoader, wrapper, unwrapper, finder));
+    }
+
+    public static void validateMirrorDefinition(Class<?> mirrorDefinition) {
+        validateMirrorDefinition(mirrorDefinition, mirrorDefinition.getClassLoader());
+    }
+
+    public static void validateMirrorDefinition(Class<?> mirrorDefinition, ClassLoader targetClassLoader) {
+        Unwrapper unwrapper = new Unwrapper(targetClassLoader);
+        ReflectionFinder finder = new ReflectionFinder(targetClassLoader, unwrapper);
+        MirrorValidator validator = new MirrorValidator(finder, mirrorDefinition);
+        validator.validate();
+    }
+
+    private Mirror() {
+        // don't intantiate
+    }
+}
---
+++ src/com/genymobile/mirror/ReflectionFinder.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016 Genymobile
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.genymobile.mirror;
+
+import com.genymobile.mirror.annotation.SetInstance;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Helper class to find internal fields and methods via reflection.
+ */
+class ReflectionFinder {
+
+    private ClassLoader classLoader;
+
+    private Unwrapper unwrapper;
+
+    ReflectionFinder(ClassLoader classLoader, Unwrapper unwrapper) {
+        this.classLoader = classLoader;
+        this.unwrapper = unwrapper;
+    }
+
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+        return Class.forName(name, true, classLoader);
+    }
+
+    public Field findField(Class<?> clazz, String name) throws NoSuchFieldException {
+        return clazz.getDeclaredField(name);
+    }
+
+    public Method findMethod(Class<?> clazz, Method mirrorMethod) throws NoSuchMethodException {
+        return findMethod(clazz, mirrorMethod.getName(), retrieveParameters(mirrorMethod));
+    }
+
+    public Method findSetInstanceMethod(Class clazz) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (isSetInstanceMethods(method)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    private boolean isSetInstanceMethods(Method method) {
+        return method.getAnnotation(SetInstance.class) != null;
+    }
+
+    private Method findMethod(Class<?> clazz, String name, Class<?>... params) throws NoSuchMethodException {
+        return clazz.getDeclaredMethod(name, params);
+    }
+
+    public Constructor<?> findConstructor(Class<?> clazz, Method mirrorMethod) throws NoSuchMethodException {
+        return findConstructor(clazz, retrieveParameters(mirrorMethod));
+    }
+
+    private Constructor<?> findConstructor(Class<?> clazz, Class<?>... params) throws NoSuchMethodException {
+        return clazz.getDeclaredConstructor(params);
+    }
+
+    private Class[] retrieveParameters(Method method) {
+        Class[] genuineTypes = method.getParameterTypes();
+        Class[] types = new Class[genuineTypes.length];
+
+        for (int i = 0; i < genuineTypes.length; ++i) {
+            types[i] = unwrapper.unwrapClass(genuineTypes[i]);
+        }
+        return types;
+    }
+
+}
